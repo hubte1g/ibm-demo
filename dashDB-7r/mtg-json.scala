d1b32b0308c4090d2dffffc077b385af9c2ff4d5
hubte1g <lickteig.joshua@gmail.com>
2016-06-12 14:56:15 -0500

    Update mtg-json.scala

--- dashDB-7r/mtg-json.scala
+++ dashDB-7r/mtg-json.scala
@@ -24,9 +24,33 @@ dfRDD.take(3).foreach(println)
 
 // Create a Spark SQL temp table
 // Note that temp tables will not persist across cluster restarts
+//TEST SAMPLE
 df.registerTempTable("test_json")
-val ee = sqlContext.sql("select employee_code, tm_of_dy_ind, count(mtg_id) as mtg_ct, sum(mtg_dur) as dur_ttl from test_json group by employee_code, tm_of_dy_ind order by mtg_ct desc")
+//val ee = sqlContext.sql("select employee_code, tm_of_dy_ind, count(mtg_id) as mtg_ct, sum(mtg_dur) as dur_ttl from test_json group by employee_code, tm_of_dy_ind order by mtg_ct desc")
+
+val ee = sqlContext.sql("select mtg_loc, mtg_tag, employee_code, mtg_dte, tm_of_dy_ind, count(mtg_id) as mtg_ct, sum(mtg_dur) as mtg_dur_ttl, sum(mtg_accptd_ct) as accptd_ttl, sum(mtg_dclnd_ct) as dclnd_ttl, sum(mtg_invtd_ct) as invtd_ttl from test_json group by mtg_loc, mtg_tag, employee_code, mtg_dte, tm_of_dy_ind order by mtg_ct desc")
 ee.show
+
+//write to hdfs part files
+val ee_out=ee.collect()
+val ee_out_rdd=sc.parallelize(ee_out)
+ee_out_rdd.saveAsTextFile("C:/Users/Kizmet/Downloads/ibm-demo/data/out/")
+
+%unix
+sed 's/.//;s/.$//' 'C:/Users/Kizmet/Downloads/ibm-demo/data/out/part-00000' 
+
+
+/***not working
+import java.io._
+def printToFile(f: java.io.File)(op: java.io.PrintWriter => Unit) {
+  val p = new java.io.PrintWriter(f)
+  try { op(p) } finally { p.close() }}
+
+printToFile(new File("C:/Users/Kizmet/Documents/rgd/scix/ee_out_rdd.txt"))(p => { 
+	ee_out_rdd.map{ x => x._1}.foreach(p.println);
+	})
+	***/
+
 
 //schema ref:
 root
