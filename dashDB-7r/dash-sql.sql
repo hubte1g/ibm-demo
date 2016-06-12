ecc6764a0eb69dfc1ae0dcb659ceb9651dfd6154
hubte1g <lickteig.joshua@gmail.com>
2016-06-12 15:40:00 -0500

    Create dash-sql.sql

---
+++ dashDB-7r/dash-sql.sql
@@ -0,0 +1,2 @@
+select * from DASH6973.MTG_DTE_EMPLOYEE_LOC a11 join GOSALESHR.EMPLOYEE a12 on a12.employee_code=a12.employee_code
+select a11.employee_code, count(mtg_ct) from DASH6973.MTG_DTE_EMPLOYEE_LOC a11 join GOSALESHR.EMPLOYEE a12 on a12.employee_code=a12.employee_code group by a11.employee_code
