83f653e7a1cd38ef7740a680f3092ae15b25642e
hubte1g <lickteig.joshua@gmail.com>
2016-06-12 15:10:46 -0500

    Update mtg-json.scala

--- dashDB-7r/mtg-json.scala
+++ dashDB-7r/mtg-json.scala
@@ -37,8 +37,9 @@ val ee_out_rdd=sc.parallelize(ee_out)
 ee_out_rdd.saveAsTextFile("C:/Users/Kizmet/Downloads/ibm-demo/data/out/")
 
 %unix
-sed 's/.//;s/.$//' 'C:/Users/Kizmet/Downloads/ibm-demo/data/out/part-00000' 
-
+cat part-00000 part-00001 part-00002 part-00003 > merged.txt
+//sed 's/.//;s/.$//' 'C:/Users/Kizmet/Downloads/ibm-demo/data/out/part-00000' 
+sed 's/.//;s/.$//' 'C:/Users/Kizmet/Downloads/ibm-demo/data/out/merged.txt' 
 
 /***not working
 import java.io._
